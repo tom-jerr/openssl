e272e3b7460f56afa1f8d8694a87f116e1671f29
tom-jerr <2584074296@qq.com>
2022-11-28 13:28:54 +0800

    mod_exp修改3.0

--- BN_mod_exp.cpp
+++ BN_mod_exp.cpp
@@ -10,51 +10,56 @@ using namespace std;
 //  返回值:
 //      string类型, 返回计算的结果, 表示为10进制数字符串
 string mod_exp(string a, string e, string m) {
-	//BIGNUM* a1; BIGNUM* a2; BIGNUM* e1; BIGNUM* m1; BIGNUM* ans; BIGNUM* temp;
-	//BN_CTX *ctx=NULL;
-	//ctx=BN_CTX_new();
-	//BN_CTX_start(ctx);
+	/*
+		使用大数基本运算实现mod_exp运算
+	*/
+	
+	BIGNUM* a1; BIGNUM* a2; BIGNUM* e1; BIGNUM* m1; BIGNUM* ans; BIGNUM* temp;
+	BN_CTX *ctx=NULL;
+	ctx=BN_CTX_new();
+	BN_CTX_start(ctx);
 
-	//a1 = BN_new(); a2 = BN_new(); e1 = BN_new(); m1 = BN_new(); ans = BN_new(); temp = BN_new();
-	//BN_dec2bn(&a1, a.c_str()); BN_dec2bn(&a2, a.c_str()); BN_dec2bn(&e1, e.c_str()); BN_dec2bn(&m1, m.c_str());
-	//BN_one(ans);
-	//int length = BN_num_bits(e1); int flag = 0;
-	//while ((length - 1) > 0){
-	//	BN_mul(ans,ans,ans,ctx); 
-	//	BN_nnmod(ans,ans,m1,ctx);
-	//	BN_copy(temp, e1);
-	//	int length1 = BN_num_bits(temp);
-	//	
-	//	if (length1 != length - 1 && flag == 1) {
-	//		length = length - 1;
-	//		BN_lshift(e1, e1, length - length1);
-	//		BN_set_bit(e1, 0);
-	//		for (int i = 1; i <= length - length1 ; i++) {
-	//			BN_clear_bit(e1, i);
-	//		}
-	//		BN_copy(temp, e1);
-	//	}
-	//	else {
-	//		length = length1;
-	//		flag = 1;
-	//	}
-	//	BN_rshift(temp,temp,length - 1);
-	//	if (BN_is_one(temp)){
-	//		BN_mul(ans,a1,ans,ctx);
-	//		BN_nnmod(ans,ans,m1,ctx);
-	//	}
-	//	BN_mask_bits(e1,length - 1);
-	//}
-	//if (BN_is_zero(e1)) {
-	//	BN_mul(ans, ans, ans, ctx);
-	//	BN_nnmod(ans, ans, m1, ctx);
-	//}
-	////实现BN的快速幂乘法
-	//char* result = BN_bn2dec(ans);
-	//BN_CTX_end(ctx);
-	//BN_CTX_free(ctx);
-	//BN_free(a1);BN_free(e1);BN_free(m1);BN_free(ans);BN_free(temp);
-	//return result;
+	a1 = BN_new(); a2 = BN_new(); e1 = BN_new(); m1 = BN_new(); ans = BN_new(); temp = BN_new();
+	BN_dec2bn(&a1, a.c_str()); BN_dec2bn(&a2, a.c_str()); BN_dec2bn(&e1, e.c_str()); BN_dec2bn(&m1, m.c_str());
+	BN_one(ans);
+	unsigned long long length = (unsigned long long)BN_num_bits(e1); int flag = 0;
+	while ((length - 1) > 0){
+		BN_mul(ans,ans,ans,ctx); 
+		BN_nnmod(ans,ans,m1,ctx);
+		BN_copy(temp, e1);
+		int length1 = BN_num_bits(temp);
+		
+		if (length1 != length - 1 && flag == 1) {
+			length = length - 1;
+			BN_lshift(e1, e1, length - length1);
+			BN_set_bit(e1, length1 - 1);
+			for (unsigned long long i = length1 ; i <= length - 1 ; i++) {
+				BN_clear_bit(e1, (int)i);
+			}
+			BN_copy(temp, e1);
+		}
+		else {
+			length = length1;
+			flag = 1;
+		}
+		BN_rshift(temp,temp,length - 1);
+		BN_mask_bits(temp, 1);
+		if (BN_is_one(temp)){
+			BN_mul(ans,a1,ans,ctx);
+			BN_nnmod(ans,ans,m1,ctx);
+		}
+		BN_mask_bits(e1,length - 1);
+	}
+	
+	char* result = BN_bn2dec(ans);
+	BN_CTX_end(ctx);
+	BN_CTX_free(ctx);
+	BN_free(a1);BN_free(e1);BN_free(m1);BN_free(ans);BN_free(temp);
+	return result;
+
+	/*
+		BN大数库中标准计算mod_exp的函数
+	*/
 
 	/*BIGNUM* a1; BIGNUM* e1; BIGNUM* m1; BIGNUM* res;
 	int flag=0;
@@ -78,7 +83,7 @@ string mod_exp(string a, string e, string m) {
 	/*
 		纯暴力方式，大数运算耗时过长
 	*/
-	BIGNUM* a1; BIGNUM* a2; BIGNUM* e1; BIGNUM* m1; BIGNUM* ans; BIGNUM* temp;
+	/*BIGNUM* a1; BIGNUM* a2; BIGNUM* e1; BIGNUM* m1; BIGNUM* ans; BIGNUM* temp;
 	BN_CTX *ctx=NULL;
 	ctx=BN_CTX_new();
 	BN_CTX_start(ctx);
@@ -95,7 +100,7 @@ string mod_exp(string a, string e, string m) {
 	BN_CTX_end(ctx);
 	BN_CTX_free(ctx);
 	BN_free(a1);BN_free(e1);BN_free(m1);BN_free(ans);BN_free(temp);
-	return result;
+	return result;*/
 	
 }
 
@@ -141,9 +146,11 @@ string mod_inverse(string a, string m) {
 
 int main(){
 	/*string result1 = mod_exp("82019154470699086128524248488673846867876336512717", "82019154470699",
-	                   "8201915447069908612852424848867384686787636512717");*/
+	                   "8201915447069908612852424848867384686787636512717");
+	printf("%s\n", result1.c_str());*/
 	string result = mod_exp("23","2","100");
 	printf("%s\n", result.c_str());
+	
 	if (result == "29"){
 		printf("This is True\n");
 		return 0;
